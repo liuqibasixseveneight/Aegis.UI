0d09769c329421bbad3da631c3ddb8d954fc86a1
liuqiba <85908203+liuqibasixseveneight@users.noreply.github.com>
2023-12-23 16:19:59 +0000

    feature: Styling for active navigation links (#7)

--- src/components/ui/organisms/Navigation/Navigation.tsx
+++ src/components/ui/organisms/Navigation/Navigation.tsx
@@ -1,15 +1,23 @@
-import { type FC } from "react";
-import { List, ListItem } from "@chakra-ui/react";
+import { type FC } from 'react';
+import { List, ListItem } from '@chakra-ui/react';
+import { useLocation } from 'react-router-dom';
 
-import { NavigationProps } from "./types";
-import { NavigationItem } from "../../../ui";
+import { NavigationProps } from './types';
+import { NavigationItem } from '../../../ui';
 
 const Navigation: FC<NavigationProps> = ({ listItems, isOpen }) => {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
     <List>
       {listItems?.map((item, index) => (
         <ListItem key={`${index}-${item?.label}`}>
-          <NavigationItem item={item} isActive={index === 0} isOpen={isOpen} />
+          <NavigationItem
+            item={item}
+            isActive={pathname === item?.path}
+            isOpen={isOpen}
+          />
         </ListItem>
       ))}
     </List>
